3e6083d996a1061db6f4c8f18b89564e3678230d
AxelNegro <51132582+AxelNegro@users.noreply.github.com>
2019-10-24 19:04:57 -0300

    UsuarioDao

---
+++ Hospital_Frenz/src/datos/UsuarioDao.java
@@ -0,0 +1,14 @@
+package datos;
+
+import java.util.List;
+
+import entidad.Usuario;
+
+public interface UsuarioDao {
+	public List<Usuario> obtenerTodas();
+	public List<Usuario> obtenerTodas(String Consulta);
+	public Usuario obtenerUna(int dni);
+	public boolean eliminar(int dni);
+	public boolean insertar(Usuario user);
+	public boolean editar(Usuario user);
+}
