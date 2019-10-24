ea7dd2c677f8069a9990120e4c6e16591a0c71e4
AxelNegro <51132582+AxelNegro@users.noreply.github.com>
2019-10-24 19:06:03 -0300

    Usuario Neg

---
+++ Hospital_Frenz/src/negocio/UsuarioNeg.java
@@ -0,0 +1,14 @@
+package negocio;
+
+import java.util.ArrayList;
+
+import entidad.Usuario;
+
+public interface UsuarioNeg {
+	public ArrayList<Usuario> listarUsuarios();
+	public ArrayList<Usuario> listarUsuarios(String Consulta);
+	public Usuario obtenerUsuario(int dni);
+	public boolean borrar(int dni);
+	public boolean insertar(Usuario user);
+	public boolean editar(Usuario user);
+}
