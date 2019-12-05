e0c7f36421b19b5862542d858704a26ee79f97e1
AxelNegro <51132582+AxelNegro@users.noreply.github.com>
2019-12-05 15:14:13 -0300

    Agregado js que Obtiene Modificación
    Se encarga de armar la consulta para realizar la modificación

---
+++ Hospital_Frenz/WebContent/javascript/ObtenerModificacion.js
@@ -0,0 +1,20 @@
+function ObtenerModificacion(boton,tablaID){
+
+	var indice=boton.parentNode.parentNode.rowIndex;
+	var x=document.getElementById(tablaID).rows;
+	var y=x[indice].cells;
+	var consulta;	
+
+	if(tablaID=="tbUsers")
+	{
+		var DNI=document.getElementById("lblDNI["+indice+"]").value;
+		var Contrasenia=document.getElementById("lblContrasenia["+indice+"]").value;		
+		var Email=document.getElementById("lblEmail["+indice+"]").value;
+		var Tipo=document.getElementById("lblTipo["+indice+"]").value;
+		
+		consulta="UPDATE USUARIOS SET ContraseniaUser = '"+Contrasenia+"', EmailUser ='"+Email+"', TipoUser ='"+Tipo+"' WHERE DNI LIKE '"+DNI+"'";	
+		
+	}
+
+	return consulta;
+}
