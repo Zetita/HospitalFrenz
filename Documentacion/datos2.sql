46c61dc7f21b41a44fc0baf380b09cefcb6107e0
AxelNegro <51132582+AxelNegro@users.noreply.github.com>
2019-12-09 22:41:47 -0300

    Cambiecitos datos2
    Cambiado datos2 de adm, med y pac a Administrador, Medico y Paciente, respectivamente
    NO SE OLVIDEN DE EJECUTAR LA CONSULTA DE WPP
    use hospitalfrenz;
    alter table usuarios
    modify TipoUser varchar(20) not null

--- Documentacion/datos2.sql
+++ Documentacion/datos2.sql
@@ -1,4 +1,4 @@
-INSERT INTO `hospitalfrenz`.`medicos` 
+﻿INSERT INTO `hospitalfrenz`.`medicos` 
 (`DNIMed`, `MatriculaMed`, `NombreMed`, `ApellidosMed`, `DireccionMed`, `IDLocalidad`, `TelefonoMed`, `EstadoMed`) 
 VALUES ('17.123.123', '11111111', 'Martha', 'Torres Hidalgo', 'Carlos Pellegrini 2700', '2510', '15-1234-1234', '1');
 INSERT INTO `hospitalfrenz`.`medicos` 
@@ -35,9 +35,9 @@ VALUES ('4', '1', '11111111', '35.000.350', '2020-01-25', '10:30:00', '2', '1', 
 
 
 INSERT INTO `hospitalfrenz`.`usuarios` (`NombreUser`, `EmailUser`, `DNIUser`, `ContraseniaUser`, `TipoUser`, `EstadoUser`) 
-VALUES ('admin', 'admin@admin.com', '00.000.000', 'admin', 'adm', TRUE);
+VALUES ('admin', 'admin@admin.com', '00.000.000', 'admin', 'Administrador', TRUE);
 
 INSERT INTO `hospitalfrenz`.`usuarios` (`NombreUser`, `EmailUser`, `DNIUser`, `ContraseniaUser`, `TipoUser`, `EstadoUser`) 
-VALUES ('martha.th', 'martha@yahoo.com', '17.123.123', '12345', 'med', TRUE);
+VALUES ('martha.th', 'martha@yahoo.com', '17.123.123', '12345', 'Medico', TRUE);
 INSERT INTO `hospitalfrenz`.`usuarios` (`NombreUser`, `EmailUser`, `DNIUser`, `ContraseniaUser`, `TipoUser`, `EstadoUser`) 
-VALUES ('alexis.m', 'alexis.m@gmail.com', '35.000.350', '1212', 'pac', TRUE);
+VALUES ('alexis.m', 'alexis.m@gmail.com', '35.000.350', '1212', 'Paciente', TRUE);
