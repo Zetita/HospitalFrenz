e6c6ae82a3264ae665ea830ba5c7dd939ab9e311
AxelNegro <51132582+AxelNegro@users.noreply.github.com>
2019-12-12 03:47:51 -0300

    Add files via upload

--- Hospital_Frenz/src/datos/EspecialidadDao.java
+++ Hospital_Frenz/src/datos/EspecialidadDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import entidad.Especialidad;
 
 public interface EspecialidadDao {
-	public List<Especialidad> obtenerTodas();
+	public List<Especialidad> obtenerTodas(int Origen);
 	public Especialidad obtenerUna(int id);
 	public boolean insertar(Especialidad esp);
 	public boolean editar(Especialidad esp);
