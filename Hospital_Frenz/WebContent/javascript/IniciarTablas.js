deb6f372466f9af661c444a1e01b0cc1c32dcf9f
AxelNegro <51132582+AxelNegro@users.noreply.github.com>
2019-12-12 03:49:44 -0300

    Add files via upload

--- Hospital_Frenz/WebContent/javascript/IniciarTablas.js
+++ Hospital_Frenz/WebContent/javascript/IniciarTablas.js
@@ -4,7 +4,7 @@ $(document).ready( function () {
 		            "sProcessing":     "Procesando...",
 	                "sLengthMenu":     "Mostrar _MENU_ registros",
 	                "sZeroRecords":    "No se encontraron resultados",
-	                "sEmptyTable":     "Ningún dato disponible en esta tabla =(",
+	                "sEmptyTable":     "Ningï¿½n dato disponible en esta tabla =(",
 	                "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
 	                "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
 	                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
@@ -15,7 +15,7 @@ $(document).ready( function () {
 	                "sLoadingRecords": "Cargando...",
 	                "oPaginate": {
 	                    "sFirst":    "Primero",
-	                    "sLast":     "Último",
+	                    "sLast":     "ï¿½ltimo",
 	                    "sNext":     "Siguiente",
 	                    "sPrevious": "Anterior"
 	                },
@@ -30,3 +30,36 @@ $(document).ready( function () {
 				}
 		    } );
 		} );
+
+$(document).ready( function () {
+    $('#tbMedicos').DataTable( {
+        "language": {
+            "sProcessing":     "Procesando...",
+            "sLengthMenu":     "Mostrar _MENU_ registros",
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningï¿½n dato disponible en esta tabla =(",
+            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Buscar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "ï¿½ltimo",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "buttons": {
+                "copy": "Copiar",
+                "colvis": "Visibilidad"
+            }
+		}
+    } );
+} );
